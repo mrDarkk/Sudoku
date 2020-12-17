daa7c032a2b4a83fab796da3e29d4a642181f069
bhupendrabanothe <bhupendrabanothe@gmail.com>
2020-12-17 19:29:26 +0530

    fix: validation with sudoku

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ var board = [
 
 
 function validRow(board) {
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     let x = 0
     for (var i = 0; i < 9; i++) {
         for (var j = 0; j < 9; j++) {
+            // for(var)
+            // console.log(arr[i]);
             if (board[i][j] === 1) {
                 //console.log([i, j]);
                 x++;
@@ -29,9 +32,117 @@ function validRow(board) {
     }
 
     // return [-1, -1];
-    // console.log(x);
+    console.log(x);
     return x;
 }
+
+
+const isValidSudoku = (board) => {
+    const obj = {
+        rows: {}, // 9 rows, each containing 9 cells across
+        columns: {}, // 9 columns, each containing 9 cells down
+        blocks: {}, // 9 blocks, each containing 3x3 cells inside
+    };
+
+    let isValid = true;
+
+    loop1:
+        for (let r = 0; r < board.length; r++) {
+
+            loop2: for (let c = 0; c < board[r].length; c++) {
+
+                const cellValue = board[r][c];
+
+                if (cellValue === 0
+
+                ) {
+                    continue;
+                }
+
+                // 1: validate rows
+                if (!obj.rows.hasOwnProperty(r)) {
+                    obj.rows[r] = {};
+                }
+
+                if (obj.rows[r].hasOwnProperty(cellValue)) {
+                    isValid = false;
+                    break loop1;
+                } else {
+                    obj.rows[r][cellValue] = null;
+                }
+
+                // 2: validate columns
+                if (!obj.columns.hasOwnProperty(c)) {
+                    obj.columns[c] = {};
+                }
+
+                if (obj.columns[c].hasOwnProperty(cellValue)) {
+                    isValid = false;
+                    break loop1;
+                } else {
+                    obj.columns[c][cellValue] = null;
+                }
+
+                // 3: validate grid
+                const b = getBlockNumber(r, c);
+
+                if (!obj.blocks.hasOwnProperty(b)) {
+                    obj.blocks[b] = {};
+                }
+
+                if (obj.blocks[b].hasOwnProperty(cellValue)) {
+                    isValid = false;
+                    break loop1;
+                } else {
+                    obj.blocks[b][cellValue] = null;
+                }
+            }
+        }
+
+    return isValid;
+};
+
+// if row = 8 & column = 5
+// then quotients = 2, 1
+// therefore blockNumber = 8
+const getBlockNumber = (row, column) => {
+    const rowQuotient = parseInt(row / 3, 10);
+    const columnQuotient = parseInt(column / 3, 10);
+
+    let blockNumber;
+
+    switch (`${rowQuotient}${columnQuotient}`) {
+        case '00':
+            blockNumber = 0;
+            break;
+        case '01':
+            blockNumber = 1;
+            break;
+        case '02':
+            blockNumber = 2;
+            break;
+        case '10':
+            blockNumber = 3;
+            break;
+        case '11':
+            blockNumber = 4;
+            break;
+        case '12':
+            blockNumber = 5;
+            break;
+        case '20':
+            blockNumber = 6;
+            break;
+        case '21':
+            blockNumber = 7;
+            break;
+        case '22':
+            blockNumber = 8;
+            break;
+    };
+
+    return blockNumber;
+};
 
 
 function nextEmptySpot(board) {
@@ -122,12 +233,19 @@ function solve(board) {
 
 //console.log(solve(board));
 //console.log(validRow(board));
-const o = validRow(board);
-console.log(o);
+//const o = validRow(board);
+// console.log(o);
 
-if (5 > o) {
+//console.log(isValidSudoku(board));
+
+if (isValidSudoku(board) == true) {
     console.log(solve(board));
 } else {
-    console.log("invaild");
+    console.log("invalid sudoku !!!")
 }
 
+// if (5 > o) {
+//     console.log(solve(board));
+// } else {
+//     console.log("invaild sudoku please check !!!");
+// }
